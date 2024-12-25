8cdc764a5ebeaec38198196c61f8bd781f9a3a47
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-25 09:30:44 +0300

    feat:init project.

---
+++ src/main/java/com/springdataprojection/SpringDataProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.springdataprojection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataProjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataProjectionApplication.class, args);
+    }
+
+}
